613e9a06129d1ee60156964142c909c3b6bcde13
Toni Neubert <neubertt@eppendorf.de>
2017-07-19 13:11:48 +0200

    Deploy6

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ MONTH=$(date +"%m")
 git config --global user.email "${GIT_EMAIL}"
 git config --global user.name "${GIT_NAME}"
 git config --global push.default simple
-git remote add origin https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git
+#git remote add origin https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git
 export GIT_TAG=V2.$YEAR-$MONTH.$TRAVIS_BUILD_NUMBER
 git fetch --tags
 msg="Tag Generated from TravisCI for build $TRAVIS_BUILD_NUMBER"
